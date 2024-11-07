05dca1af116288a8dcaf31e42fa65d9a50580dd5
Winzinger, Jonas <j.winzinger@pfaffinger.com>
2024-11-07 13:35:22 +0100

    Preperation for Account system

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import About from "./components/About";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
+import Start from './Pages/Start';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
-    <>
-    <div className="blob-outer-container md:rounded-[9999px] rounded-[9999px] md:blur-[321px] blur-[200px] opacity-[60] md:opacity-[0.5]">
-      <div className="blob-inner-container">
-        <div className="blob"></div>
-      </div>
-    </div>
-    <NavBar />
-    <main>
-      <Home />
-      <About />
-      <Projects />
-    </main>
-    <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Start/>}/>
+        {/*<Route path='/login' element={<Start/>}/>*/}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Start.jsx
@@ -0,0 +1,27 @@
+import About from "../components/About";
+import Home from "../components/Home";
+import NavBar from "../components/NavBar";
+import Projects from "../components/Projects";
+import Footer from "../components/Footer";
+
+
+function Start() {
+  return (
+    <>
+    <div className="blob-outer-container md:rounded-[9999px] rounded-[9999px] md:blur-[321px] blur-[200px] opacity-[60] md:opacity-[0.5]">
+      <div className="blob-inner-container">
+        <div className="blob"></div>
+      </div>
+    </div>
+    <NavBar />
+    <main>
+      <Home />
+      <About />
+      <Projects />
+    </main>
+    <Footer />
+    </>
+  );
+}
+
+export default Start;
