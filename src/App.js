d5dbc545d3c466fbbc266c36a03af6cbe5498820
Winzinger, Jonas <j.winzinger@pfaffinger.com>
2024-11-07 07:56:29 +0100

    bg color, animation adn mobile adjustments

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-    <div className="blob-outer-container opacity-[1] md:opacity-[0.6]">
+    <div className="blob-outer-container md:rounded-[9999px] rounded-[9999px] md:blur-[321px] blur-[200px] opacity-[60] md:opacity-[0.5]">
       <div className="blob-inner-container">
         <div className="blob"></div>
       </div>
