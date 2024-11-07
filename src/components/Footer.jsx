76a0ac31beca7c4487be584bc99efe877fd94159
Winzinger, Jonas <j.winzinger@pfaffinger.com>
2024-11-07 10:57:22 +0100

    npm update and code update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='flexCenter mt-10 md:mx-[7%]'>
         <div className='padding-container max-container flex w-full flex-col gap-14'>
-            <div className="border-2 border-black border-opacity-[10%] border-dashed" />
+            <div className="border-2 border_b border-dashed" />
             <p className='regular-14 w-full text-center text-zinc-200 text-opacity-[50%] hover:text-opacity-[80%] font-semibold duration-200 mb-10'>&copy; {new Date().getFullYear()} Cavvoh | All rights reserved</p>
         </div>
     </footer>
